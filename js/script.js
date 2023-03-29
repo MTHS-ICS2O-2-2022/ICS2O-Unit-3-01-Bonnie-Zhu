7d9af65844ffca4dc119c127b11297a582600afa
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-03-29 18:33:12 +0000

    html,css,javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie Zhu
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+* This function calculates your pay depending on the amount of hours you work (including tax).
+*/
+function calculate() {
+  // input
+  const Base1 = parseInt(document.getElementById('Base1').value)
+  const Base2 = parseInt(document.getElementById('Base2').value)
+  const Height = parseInt(document.getElementById('Height').value)
+
+  // process
+  const Area = (Base1 + Base2) /2 * Height
+
+  // output
+  document.getElementById('Area').innerHTML = 'Area is: ' + Area + 'mm²'
+}
+
